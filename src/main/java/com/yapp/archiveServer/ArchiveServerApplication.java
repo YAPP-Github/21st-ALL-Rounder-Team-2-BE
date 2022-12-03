56fc5060451497511627cb0bed544d4a2e8012a1
leesky <leehaneul0623@gmail.com>
2022-12-03 22:10:04 +0900

    chore : mysql 데이터베이스 설정
    spring jpa 연동을 위한 데이터베이스 설정 :
     - docker-compose 파일 생성
     - mysql 드라이버 종속성 추가
     - application.yml 생성
     - 진입 구간, 데이터소스 무시 제거

--- src/main/java/com/yapp/archiveServer/ArchiveServerApplication.java
+++ src/main/java/com/yapp/archiveServer/ArchiveServerApplication.java
@@ -1,12 +1,9 @@
 package com.yapp.archiveServer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ArchiveServerApplication {
 
     public static void main(String[] args) {
