556b0045a76758a4badae621fb8dedccb20a98fa
akalswl14 <gnkryd12@gmail.com>
2023-01-13 12:46:40 +0900

    feat : Artwork가 빌더 생성자 패턴을 쓰도록 변경

--- src/main/java/com/yapp/artie/domain/archive/domain/artwork/ArtworkContents.java
+++ src/main/java/com/yapp/artie/domain/archive/domain/artwork/ArtworkContents.java
@@ -3,25 +3,57 @@ package com.yapp.artie.domain.archive.domain.artwork;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ArtworkContents {
 
-  @Column(nullable = false)
+  @Column
   private String name;
 
-  @Column(nullable = false)
+  @Column
   private String artist;
 
   @Column(nullable = false)
-  private String url;
+  private String uri;
 
-  @Column(columnDefinition = "text")
-  private String description;
+  private ArtworkContents(Builder builder) {
+    name = builder.name;
+    artist = builder.artist;
+    uri = builder.uri;
+  }
+
+  public static class Builder {
+
+    // 필수 매개변수
+    private final String uri;
+
+    // 선택 매개변수
+    private String name;
+    private String artist;
+
+    // 필수 매개변수만을 담은 Builder 생성자
+    public Builder(String uri) {
+      this.uri = uri;
+    }
+
+    // 선택 매개변수의 setter, Builder 자신을 반환해 연쇄적으로 호출 가능
+    public Builder name(String val) {
+      this.name = val;
+      return this;
+    }
+
+    public Builder artist(String val) {
+      this.artist = val;
+      return this;
+    }
+
+    // build() 호출로 최종 불변 객체를 얻는다.
+    public ArtworkContents build() {
+      return new ArtworkContents(this);
+    }
+  }
 }
