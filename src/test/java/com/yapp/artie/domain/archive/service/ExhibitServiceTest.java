f94542445c4930fd1863a4e35064439224acf929
leesky <leehaneul0623@gmail.com>
2023-01-22 23:39:53 +0900

    test : 전시 생성 테스트 구현

--- src/test/java/com/yapp/artie/domain/archive/service/ExhibitServiceTest.java
+++ src/test/java/com/yapp/artie/domain/archive/service/ExhibitServiceTest.java
@@ -1,6 +1,13 @@
 package com.yapp.artie.domain.archive.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.yapp.artie.domain.archive.domain.exhibit.Exhibit;
+import com.yapp.artie.domain.archive.dto.exhibit.CreateExhibitRequestDto;
+import com.yapp.artie.domain.user.domain.User;
 import com.yapp.artie.domain.user.repository.UserRepository;
+import java.time.LocalDate;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.Test;
@@ -24,14 +31,29 @@ class ExhibitServiceTest {
   @Autowired
   ExhibitService exhibitService;
 
+  @Autowired
+  CategoryService categoryService;
+
+  User createUser() {
+    User user = new User();
+    user.setName("test");
+    user.setUid("tu1");
+    em.persist(user);
+    categoryService.createDefault(user.getId());
+
+    return user;
+  }
 
   @Test
-  public void test() throws Exception {
-    //given
+  public void create_전시를_생성한다() throws Exception {
+    User user = createUser();
+    CreateExhibitRequestDto exhibitRequestDto = new CreateExhibitRequestDto("test", 1L,
+        LocalDate.now());
 
-    //when
+    exhibitService.create(exhibitRequestDto, user.getId());
+    Optional<Exhibit> exhibit = Optional.ofNullable(em.find(Exhibit.class, 1L));
 
-    //then
+    assertThat(exhibit).isPresent();
   }
 
 }
