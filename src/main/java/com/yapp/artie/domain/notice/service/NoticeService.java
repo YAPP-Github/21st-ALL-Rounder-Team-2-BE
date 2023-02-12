6006ffad7af4057f9db432c796c57d3b93af2748
leesky <leehaneul0623@gmail.com>
2023-02-13 03:21:55 +0900

    feat : 공지사항 상세 조회 기능 구현

--- src/main/java/com/yapp/artie/domain/notice/service/NoticeService.java
+++ src/main/java/com/yapp/artie/domain/notice/service/NoticeService.java
@@ -1,6 +1,9 @@
 package com.yapp.artie.domain.notice.service;
 
+import com.yapp.artie.domain.notice.domain.Notice;
+import com.yapp.artie.domain.notice.dto.NoticeDetailInfo;
 import com.yapp.artie.domain.notice.dto.NoticeDto;
+import com.yapp.artie.domain.notice.exception.NoticeNotFoundException;
 import com.yapp.artie.domain.notice.repository.NoticeRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +19,15 @@ public class NoticeService {
 
   public List<NoticeDto> notices() {
     return noticeRepository.findNoticeDto();
+  }
+
+  public NoticeDetailInfo notice(Long id) {
+    Notice notice = noticeRepository.findById(id).orElseThrow(NoticeNotFoundException::new);
+    return new NoticeDetailInfo(
+        notice.getId(),
+        notice.getCreatedAt(),
+        notice.getTitle(),
+        notice.getContents()
+    );
   }
 }
