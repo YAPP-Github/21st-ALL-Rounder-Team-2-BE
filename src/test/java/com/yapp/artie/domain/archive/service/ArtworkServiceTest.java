e598954a90f54c70c98c59a1ed753c6fdf31a470
akalswl14 <gnkryd12@gmail.com>
2023-02-15 17:43:09 +0900

    feat : Tag 개편에 따른 테스트 추가 및 수정

--- src/test/java/com/yapp/artie/domain/archive/service/ArtworkServiceTest.java
+++ src/test/java/com/yapp/artie/domain/archive/service/ArtworkServiceTest.java
@@ -7,7 +7,6 @@ import com.yapp.artie.domain.archive.domain.category.Category;
 import com.yapp.artie.domain.archive.domain.exhibit.Exhibit;
 import com.yapp.artie.domain.archive.dto.artwork.CreateArtworkRequestDto;
 import com.yapp.artie.domain.archive.dto.cateogry.CreateCategoryRequestDto;
-import com.yapp.artie.domain.archive.dto.tag.CreateArtworkTagDto;
 import com.yapp.artie.domain.archive.repository.ArtworkRepository;
 import com.yapp.artie.domain.archive.repository.CategoryRepository;
 import com.yapp.artie.domain.archive.repository.ExhibitRepository;
@@ -62,8 +61,8 @@ class ArtworkServiceTest {
     Category defaultCategory = categoryRepository.findCategoryEntityGraphById(user.getId());
     Exhibit exhibit = exhibitRepository.save(
         Exhibit.create("test", LocalDate.now(), defaultCategory, user, null));
-    List<CreateArtworkTagDto> tags = new ArrayList<>();
-    tags.add(new CreateArtworkTagDto(null, "sample-tag"));
+    List<String> tags = new ArrayList<>();
+    tags.add("sample-tag");
 
     Long artworkId = artworkService.create(
         new CreateArtworkRequestDto(exhibit.getId(), "sample-uri", "sample-artist", "sample-name",
@@ -80,22 +79,72 @@ class ArtworkServiceTest {
   }
 
   @Test
+  @DisplayName("태그 없이도 정상 작품 등록")
+  public void create_태그_없이_등록() throws Exception {
+    User user = createUser("user", "tu");
+    Category defaultCategory = categoryRepository.findCategoryEntityGraphById(user.getId());
+    Exhibit exhibit = exhibitRepository.save(
+        Exhibit.create("test", LocalDate.now(), defaultCategory, user, null));
+
+    Long artworkId = artworkService.create(
+        new CreateArtworkRequestDto(exhibit.getId(), "sample-uri", "sample-artist", "sample-name",
+            null), user.getId());
+
+    Optional<Artwork> artwork = artworkRepository.findById(artworkId);
+    assertThat(artwork.isPresent()).isTrue();
+    assertThat(artwork.get().getExhibit()).isEqualTo(exhibit);
+    assertThat(artwork.get().getContents().getUri()).isEqualTo("sample-uri");
+    assertThat(artwork.get().getContents().getArtist()).isEqualTo("sample-artist");
+    assertThat(artwork.get().getContents().getName()).isEqualTo("sample-name");
+    assertThat(artwork.get().isMain()).isTrue();
+    assertThat(exhibit.isPublished()).isTrue();
+  }
+
+  @Test
+  @DisplayName("빌더로 작품 등록")
+  public void create_빌더로_등록() throws Exception {
+    User user = createUser("user", "tu");
+    Category defaultCategory = categoryRepository.findCategoryEntityGraphById(user.getId());
+    Exhibit exhibit = exhibitRepository.save(
+        Exhibit.create("test", LocalDate.now(), defaultCategory, user, null));
+    List<String> tags = new ArrayList<>();
+    tags.add("sample-tag");
+
+    Long artworkId = artworkService.create(
+        CreateArtworkRequestDto.builder()
+            .postId(exhibit.getId())
+            .imageUri("sample-uri")
+            .tags(tags).build()
+        , user.getId());
+
+    Optional<Artwork> artwork = artworkRepository.findById(artworkId);
+    assertThat(artwork.isPresent()).isTrue();
+    assertThat(artwork.get().getExhibit()).isEqualTo(exhibit);
+    assertThat(artwork.get().getContents().getUri()).isEqualTo("sample-uri");
+    assertThat(artwork.get().getContents().getArtist()).isEqualTo(null);
+    assertThat(artwork.get().getContents().getName()).isEqualTo(null);
+    assertThat(artwork.get().isMain()).isTrue();
+    assertThat(exhibit.isPublished()).isTrue();
+  }
+
+  @Test
   @DisplayName("전시에 등록되는 첫 작품은 대표 작품으로 설정")
   public void create_첫_작품_대표_작품_설정() {
     User user = createUser("user", "tu");
     Category defaultCategory = categoryRepository.findCategoryEntityGraphById(user.getId());
     Exhibit exhibit = exhibitRepository.save(
         Exhibit.create("test", LocalDate.now(), defaultCategory, user, null));
-    List<CreateArtworkTagDto> tags = new ArrayList<>();
-    tags.add(new CreateArtworkTagDto(null, "sample-tag"));
+    List<String> tags = new ArrayList<>();
+    tags.add("sample-tag");
 
     Long artworkId1 = artworkService.create(
         new CreateArtworkRequestDto(exhibit.getId(), "sample-uri", "sample-artist", "sample-name",
             tags), user.getId());
     Long artworkId2 = artworkService.create(
-        CreateArtworkRequestDto.builder().postId(exhibit.getId()).imageUri("sample-uri")
-            .tags(new ArrayList<>()).build(),
-        user.getId());
+        CreateArtworkRequestDto.builder()
+            .postId(exhibit.getId())
+            .imageUri("sample-uri").build()
+        , user.getId());
 
     Optional<Artwork> artwork1 = artworkRepository.findById(artworkId1);
     Optional<Artwork> artwork2 = artworkRepository.findById(artworkId2);
