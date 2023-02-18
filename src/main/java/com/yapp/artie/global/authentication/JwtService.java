77cbd8f7f38ec46207405c3eb13664c800a80f27
leesky <leehaneul0623@gmail.com>
2023-02-18 23:38:16 +0900

    refactor : jwtService 인터페이스 추출

--- src/main/java/com/yapp/artie/global/authentication/JwtService.java
+++ src/main/java/com/yapp/artie/global/authentication/JwtService.java
@@ -1,65 +1,10 @@
 package com.yapp.artie.global.authentication;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
-import com.yapp.artie.global.exception.authentication.ExpiredTokenException;
-import com.yapp.artie.global.exception.authentication.InvalidTokenException;
-import com.yapp.artie.global.exception.authentication.NotExistValidTokenException;
-import com.yapp.artie.global.exception.authentication.RevokedTokenException;
-import com.yapp.artie.global.exception.common.BusinessException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class JwtService {
+public interface JwtService {
 
-  private final FirebaseAuth firebaseAuth;
+  FirebaseToken verify(String header);
 
-  public FirebaseToken verify(String header) {
-    validateHeader(header);
-    return decode(refineHeaderAsToken(header));
-  }
-
-  private void validateHeader(String header) {
-    if (header == null || !header.startsWith("Bearer ") || header.trim().equals("Bearer")) {
-      throw new NotExistValidTokenException();
-    }
-  }
-
-  private FirebaseToken decode(String token) {
-    try {
-      return firebaseAuth.verifyIdToken(token);
-    } catch (IllegalArgumentException e) {
-      // If the token is null, empty,
-      // or if the FirebaseApp instance does not have a project ID associated with it.
-      throw new NotExistValidTokenException();
-    } catch (FirebaseAuthException e) {
-      // If an error occurs while parsing or validating the token.
-      throw processAuthException(e);
-    }
-  }
-
-  private BusinessException processAuthException(FirebaseAuthException e) {
-    switch (e.getAuthErrorCode()) {
-      case EXPIRED_ID_TOKEN:
-        return new ExpiredTokenException();
-      case REVOKED_ID_TOKEN:
-        return new RevokedTokenException();
-      default:
-        return new InvalidTokenException();
-    }
-  }
-
-  private String refineHeaderAsToken(String header) {
-    String authType = "Bearer";
-    if (header.startsWith(authType)) {
-      header = header.substring(authType.length()).trim();
-    }
-
-    return header;
-  }
+  void withdraw(String uid);
 }
