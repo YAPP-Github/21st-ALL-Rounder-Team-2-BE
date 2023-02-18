28b9748764560ba35fcd922157ebbb1051555d90
leesky <leehaneul0623@gmail.com>
2023-02-18 22:34:17 +0900

    test : 사용자 테이블 삭제 테스트 코드 작성

--- src/test/java/com/yapp/artie/domain/user/service/UserServiceTest.java
+++ src/test/java/com/yapp/artie/domain/user/service/UserServiceTest.java
@@ -1,9 +1,13 @@
 package com.yapp.artie.domain.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.yapp.artie.domain.user.domain.User;
 import com.yapp.artie.domain.user.repository.UserRepository;
+
+import com.yapp.artie.domain.user.dto.response.CreateUserResponseDto;
+import com.yapp.artie.domain.user.exception.UserNotFoundException;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +34,15 @@ class UserServiceTest {
     userService.updateUserName(user.getId(), "sample-name-2");
 
     assertThat(em.find(User.class, user.getId()).getName()).isEqualTo("sample-name-2");
+  }
+
+  @Test
+  public void delete_사용자_삭제시_테이블에서_제거된다() throws Exception {
+    CreateUserResponseDto dto = userService.register("123", "le2sky", null);
+    userService.delete(dto.id);
+
+    assertThatThrownBy(() -> {
+      userService.delete(dto.id);
+    }).isInstanceOf(UserNotFoundException.class);
   }
 }
