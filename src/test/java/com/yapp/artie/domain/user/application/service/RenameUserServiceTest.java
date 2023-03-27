08e9f017fbf84a3844d8dfcddc4e5e96b7b35eca
leesky <leehaneul0623@gmail.com>
2023-03-28 07:11:53 +0900

    test : RenameUserService 단위 테스트 작성

---
+++ src/test/java/com/yapp/artie/domain/user/application/service/RenameUserServiceTest.java
@@ -0,0 +1,57 @@
+package com.yapp.artie.domain.user.application.service;
+
+import static com.yapp.artie.common.UserTestData.defaultUser;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.yapp.artie.domain.user.domain.User;
+import com.yapp.artie.domain.user.exception.UserNotFoundException;
+import com.yapp.artie.domain.user.repository.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class RenameUserServiceTest {
+
+  private final UserRepository userRepository = Mockito.mock(UserRepository.class);
+  private final RenameUserService renameUserService = new RenameUserService(userRepository);
+
+  @Test
+  void rename_사용자를_찾을_수_없으면_예외를_발생한다() {
+    givenUserFindWillFail();
+    assertThatThrownBy(() -> renameUserService.rename(1L, "test"))
+        .isInstanceOf(UserNotFoundException.class);
+  }
+
+  @Test
+  void rename_주어진_이름으로_사용자의_이름이_변경된다() {
+    User user = defaultUser().build();
+    givenUserByReference(user);
+
+    String beforeName = userRepository
+        .findById(1L)
+        .orElseThrow()
+        .getName();
+
+    String expectedName = "tomcat";
+    renameUserService.rename(1L, expectedName);
+
+    assertThat(user.getName()).isEqualTo(expectedName);
+    assertThat(user.getName()).isNotEqualTo(beforeName);
+  }
+
+
+  private void givenUserByReference(User user) {
+    given(userRepository.findById(any()))
+        .willReturn(Optional.ofNullable(user));
+  }
+
+  private void givenUserFindWillFail() {
+    given(userRepository.findById(any()))
+        .willThrow(UserNotFoundException.class);
+  }
+
+
+}
