d22fe3aa9d40683c285ce78f7bb614dee0cd2790
leesky <leehaneul0623@gmail.com>
2023-03-28 06:26:43 +0900

    fix : CreateUserResponseDto Getter 추가

--- src/main/java/com/yapp/artie/domain/user/dto/response/CreateUserResponseDto.java
+++ src/main/java/com/yapp/artie/domain/user/dto/response/CreateUserResponseDto.java
@@ -1,8 +1,10 @@
 package com.yapp.artie.domain.user.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
 @Schema(description = "회원 Response")
+@Getter
 public class CreateUserResponseDto {
 
   @Schema(description = "아이디")
