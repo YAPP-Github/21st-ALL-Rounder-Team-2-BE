3f4e3630d5388fc2bd85753331622b5c28a516db
leesky <leehaneul0623@gmail.com>
2023-03-29 16:44:03 +0900

    chore : 추적안된 UserRepository 추가

---
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yapp.artie.domain.user.adapter.out.persistence;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserJpaEntity, Long> {
+
+  Optional<UserJpaEntity> findByUid(String uid);
+}
