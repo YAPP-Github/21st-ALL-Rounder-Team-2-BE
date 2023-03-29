cc47db1082307009db78820cc9ae5414c36a469f
leesky <leehaneul0623@gmail.com>
2023-03-30 01:02:08 +0900

    fix : @Repository 애노테이션 제거

--- src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserRepository.java
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserJpaEntity, Long> {
 
   Optional<UserJpaEntity> findByUid(String uid);
