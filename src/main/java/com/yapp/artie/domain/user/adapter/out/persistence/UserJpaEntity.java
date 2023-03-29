f628caca24a6a80e9857460913ebbeca376a5703
leesky <leehaneul0623@gmail.com>
2023-03-30 00:59:44 +0900

    fix : Entity 애노테이션으로 user 테이블에 매핑하도록 변경

--- src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserJpaEntity.java
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserJpaEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Setter;
 
 
 @Entity
+@Table(name = "user")
 @Getter
 @Setter
 @NoArgsConstructor
