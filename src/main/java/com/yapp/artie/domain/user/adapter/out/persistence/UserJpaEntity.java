ae14cd24b74b7bb367508585ae0621cc3b8ac338
leesky <leehaneul0623@gmail.com>
2023-03-29 17:38:41 +0900

    feat : UserMapper 구현

--- src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserJpaEntity.java
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserJpaEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserJpaEntity extends BaseEntity {
 
   @Id
