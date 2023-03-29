efc63b843988e18314c60dbbe6a34b9e4ef245cf
leesky <leehaneul0623@gmail.com>
2023-03-29 17:43:08 +0900

    feat : user 영속성 어댑터 쿼리 인터페이스 구현

--- src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserPersistenceAdapter.java
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserPersistenceAdapter.java
@@ -5,6 +5,7 @@ import com.yapp.artie.domain.user.application.port.out.LoadUserPort;
 import com.yapp.artie.domain.user.application.port.out.SaveUserPort;
 import com.yapp.artie.domain.user.application.port.out.UpdateUserStatePort;
 import com.yapp.artie.domain.user.domain.User;
+import com.yapp.artie.domain.user.exception.UserNotFoundException;
 import com.yapp.artie.global.annotation.PersistenceAdapter;
 import lombok.RequiredArgsConstructor;
 
@@ -18,12 +19,18 @@ class UserPersistenceAdapter implements DeleteUserPort, SaveUserPort, LoadUserPo
 
   @Override
   public User loadById(Long userId) {
-    return null;
+    UserJpaEntity userJpaEntity = userRepository.findById(userId)
+        .orElseThrow(UserNotFoundException::new);
+
+    return userMapper.mapToDomainEntity(userJpaEntity);
   }
 
   @Override
   public User loadByUid(String uid) {
-    return null;
+    UserJpaEntity userJpaEntity = userRepository.findByUid(uid)
+        .orElseThrow(UserNotFoundException::new);
+
+    return userMapper.mapToDomainEntity(userJpaEntity);
   }
 
   @Override
