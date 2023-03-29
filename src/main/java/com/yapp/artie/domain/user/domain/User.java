01cc80d5ffbb9cf09e599719a32b5d6511fc42ee
leesky <leehaneul0623@gmail.com>
2023-03-29 17:33:36 +0900

    feat : User 도메인 엔티티 구현

--- src/main/java/com/yapp/artie/domain/user/domain/User.java
+++ src/main/java/com/yapp/artie/domain/user/domain/User.java
@@ -1,5 +1,27 @@
 package com.yapp.artie.domain.user.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class User {
 
+  private final Long id;
+  private final String uid;
+  private final String profileImage;
+  private String name;
+
+  public static User withoutId(String uid, String name, String profileImage) {
+    return new User(null, uid, name, profileImage);
+  }
+
+  public static User withId(Long id, String uid, String name, String profileImage) {
+    return new User(id, uid, name, profileImage);
+  }
+
+  public void rename(String name) {
+    this.name = name;
+  }
 }
