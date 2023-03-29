1415aba5e9a66b0d0aee4391db95e5ecb90d57db
leesky <leehaneul0623@gmail.com>
2023-03-30 01:28:00 +0900

    fix : Renameservice와 테스트에서 아웃 포트 사용하도록 변경

--- src/main/java/com/yapp/artie/domain/user/application/service/RenameUserService.java
+++ src/main/java/com/yapp/artie/domain/user/application/service/RenameUserService.java
@@ -1,9 +1,9 @@
 package com.yapp.artie.domain.user.application.service;
 
-import com.yapp.artie.domain.user.adapter.out.persistence.UserJpaEntity;
-import com.yapp.artie.domain.user.adapter.out.persistence.UserRepository;
 import com.yapp.artie.domain.user.application.port.in.RenameUserUseCase;
-import com.yapp.artie.domain.user.exception.UserNotFoundException;
+import com.yapp.artie.domain.user.application.port.out.LoadUserPort;
+import com.yapp.artie.domain.user.application.port.out.UpdateUserStatePort;
+import com.yapp.artie.domain.user.domain.User;
 import com.yapp.artie.global.annotation.UseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,13 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RenameUserService implements RenameUserUseCase {
 
-  private final UserRepository userRepository;
+  private final LoadUserPort loadUserPort;
+  private final UpdateUserStatePort updateUserStatePort;
 
   @Override
   public void rename(Long userId, String name) {
-    UserJpaEntity user = userRepository.findById(userId)
-        .orElseThrow(UserNotFoundException::new);
+    User user = loadUserPort.loadById(userId);
+    user.rename(name);
 
-    user.setName(name);
+    updateUserStatePort.updateName(user);
   }
 }
--- src/test/java/com/yapp/artie/domain/user/application/service/RenameUserServiceTest.java
+++ src/test/java/com/yapp/artie/domain/user/application/service/RenameUserServiceTest.java
@@ -6,17 +6,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
-import com.yapp.artie.domain.user.adapter.out.persistence.UserJpaEntity;
+import com.yapp.artie.domain.user.application.port.out.LoadUserPort;
+import com.yapp.artie.domain.user.application.port.out.UpdateUserStatePort;
+import com.yapp.artie.domain.user.domain.User;
 import com.yapp.artie.domain.user.exception.UserNotFoundException;
-import com.yapp.artie.domain.user.adapter.out.persistence.UserRepository;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class RenameUserServiceTest {
 
-  private final UserRepository userRepository = Mockito.mock(UserRepository.class);
-  private final RenameUserService renameUserService = new RenameUserService(userRepository);
+  private final UpdateUserStatePort updateUserStatePort = Mockito.mock(UpdateUserStatePort.class);
+  private final LoadUserPort loadUserPort = Mockito.mock(LoadUserPort.class);
+  private final RenameUserService renameUserService = new RenameUserService(loadUserPort,
+      updateUserStatePort);
 
   @Test
   void rename_사용자를_찾을_수_없으면_예외를_발생한다() {
@@ -27,12 +29,11 @@ class RenameUserServiceTest {
 
   @Test
   void rename_주어진_이름으로_사용자의_이름이_변경된다() {
-    UserJpaEntity user = defaultUser().build();
+    User user = defaultUser().build();
     givenUserByReference(user);
 
-    String beforeName = userRepository
-        .findById(1L)
-        .orElseThrow()
+    String beforeName = loadUserPort
+        .loadById(1L)
         .getName();
 
     String expectedName = "tomcat";
@@ -42,14 +43,13 @@ class RenameUserServiceTest {
     assertThat(user.getName()).isNotEqualTo(beforeName);
   }
 
-
-  private void givenUserByReference(UserJpaEntity user) {
-    given(userRepository.findById(any()))
-        .willReturn(Optional.ofNullable(user));
+  private void givenUserByReference(User user) {
+    given(loadUserPort.loadById(any()))
+        .willReturn(user);
   }
 
   private void givenUserFindWillFail() {
-    given(userRepository.findById(any()))
+    given(loadUserPort.loadById(any()))
         .willThrow(UserNotFoundException.class);
   }
 }
