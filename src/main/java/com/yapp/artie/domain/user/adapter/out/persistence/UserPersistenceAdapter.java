76eb904f63cda59d34931258e180433a04c2c379
leesky <leehaneul0623@gmail.com>
2023-03-29 17:33:17 +0900

    feat : 영속성 어댑터 뼈대 코드 작성

--- src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserPersistenceAdapter.java
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserPersistenceAdapter.java
@@ -1,7 +1,43 @@
 package com.yapp.artie.domain.user.adapter.out.persistence;
 
-class UserPersistenceAdapter {
+import com.yapp.artie.domain.user.application.port.out.DeleteUserPort;
+import com.yapp.artie.domain.user.application.port.out.LoadUserPort;
+import com.yapp.artie.domain.user.application.port.out.SaveUserPort;
+import com.yapp.artie.domain.user.application.port.out.UpdateUserStatePort;
+import com.yapp.artie.domain.user.domain.User;
+import com.yapp.artie.global.annotation.PersistenceAdapter;
+import lombok.RequiredArgsConstructor;
 
+@PersistenceAdapter
+@RequiredArgsConstructor
+class UserPersistenceAdapter implements DeleteUserPort, SaveUserPort, LoadUserPort,
+    UpdateUserStatePort {
 
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
 
+  @Override
+  public User loadById(Long userId) {
+    return null;
+  }
+
+  @Override
+  public User loadByUid(String uid) {
+    return null;
+  }
+
+  @Override
+  public void delete(User user) {
+
+  }
+
+  @Override
+  public void save(User user) {
+
+  }
+
+  @Override
+  public void updateName(User user) {
+
+  }
 }
