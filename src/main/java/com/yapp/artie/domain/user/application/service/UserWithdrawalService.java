e2b904595610c071d00dc9dadc8895b06e000b56
leesky <leehaneul0623@gmail.com>
2023-03-31 01:05:21 +0900

    fix : 생성자로 넘겨주는 인자 순서 변경

--- src/main/java/com/yapp/artie/domain/user/application/service/UserWithdrawalService.java
+++ src/main/java/com/yapp/artie/domain/user/application/service/UserWithdrawalService.java
@@ -27,7 +27,7 @@ public class UserWithdrawalService implements UserWithdrawalUseCase {
     jwtService.withdraw(user.getUid());
     // TODO : 꼭 변경해야함! User 도메인을 받던지 따로 입력 모델을 만들자.
     categoryRepository.deleteAllByUser(
-        new UserJpaEntity(user.getId(), user.getUid(), user.getName(), user.getProfileImage()));
+        new UserJpaEntity(user.getId(), user.getUid(), user.getProfileImage(), user.getName()));
     deleteUserPort.delete(user);
   }
 
