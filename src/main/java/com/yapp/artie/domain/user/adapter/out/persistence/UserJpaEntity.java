5f23b1a5b09d9cd5084d9139679c7087d4568cee
leesky <leehaneul0623@gmail.com>
2023-03-31 05:04:20 +0900

    feat : uid 컬럼에 인덱스 적용

--- src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserJpaEntity.java
+++ src/main/java/com/yapp/artie/domain/user/adapter/out/persistence/UserJpaEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import lombok.Setter;
 
 
 @Entity
-@Table(name = "user")
+@Table(name = "user", indexes = @Index(name = "index_user_uid", columnList = "uid"))
 @Getter
 @Setter
 @NoArgsConstructor
