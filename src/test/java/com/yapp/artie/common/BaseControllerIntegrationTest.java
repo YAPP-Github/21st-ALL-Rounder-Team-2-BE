31b0ce246d691f0adc74fb0e1bbeef04721f30e2
leesky <leehaneul0623@gmail.com>
2023-04-01 23:20:57 +0900

    test : BaseControllerIntegrationTest given 메서드 구현

--- src/test/java/com/yapp/artie/common/BaseControllerIntegrationTest.java
+++ src/test/java/com/yapp/artie/common/BaseControllerIntegrationTest.java
@@ -1,10 +1,16 @@
 package com.yapp.artie.common;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.core.userdetails.User.builder;
+
+import com.yapp.artie.domain.user.domain.User;
 import com.yapp.artie.global.authentication.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -16,4 +22,26 @@ public abstract class BaseControllerIntegrationTest {
 
   @MockBean
   protected JwtService jwtService;
+
+  @MockBean
+  protected UserDetailsService userDetailsService;
+
+  protected void givenUserByReference(User user) {
+    loadUserDetailsWillReturnReference(user);
+    givenTestToken(user.getUid(), user.getName(), user.getProfileImage());
+  }
+
+  private void loadUserDetailsWillReturnReference(User user) {
+    given(userDetailsService.loadUserByUsername(any()))
+        .willReturn(builder()
+            .username(String.valueOf(user.getId()))
+            .password(user.getUid())
+            .authorities("user")
+            .build());
+  }
+
+  private void givenTestToken(String uid, String name, String picture) {
+    given(jwtService.verify(any()))
+        .willReturn(new TestArtieToken(uid, name, picture));
+  }
 }
--- src/test/java/com/yapp/artie/domain/user/adapter/in/web/RegisterUserControllerTest.java
+++ src/test/java/com/yapp/artie/domain/user/adapter/in/web/RegisterUserControllerTest.java
@@ -1,14 +1,12 @@
 package com.yapp.artie.domain.user.adapter.in.web;
 
-import static org.mockito.BDDMockito.any;
+import static com.yapp.artie.common.UserTestData.defaultUser;
 import static org.mockito.BDDMockito.eq;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.yapp.artie.common.BaseControllerIntegrationTest;
-import com.yapp.artie.common.TestArtieToken;
 import com.yapp.artie.domain.user.application.port.in.RegisterUserUseCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,7 +20,10 @@ class RegisterUserControllerTest extends BaseControllerIntegrationTest {
 
   @Test
   void testRegister() throws Exception {
-    givenTestToken("uid", "tomcat", "test.com");
+    givenUserByReference(defaultUser()
+        .withName("tomcat")
+        .withProfileImage("test.com")
+        .build());
 
     mvc.perform(post("/user")
         .header("Content-Type", "application/json")
@@ -32,10 +33,5 @@ class RegisterUserControllerTest extends BaseControllerIntegrationTest {
 
     then(registerUserUseCase).should()
         .register(eq("uid"), eq("tomcat"), eq("test.com"));
-  }
-
-  private void givenTestToken(String uid, String name, String picture) {
-    given(jwtService.verify(any()))
-        .willReturn(new TestArtieToken(uid, name, picture));
   }
 }
