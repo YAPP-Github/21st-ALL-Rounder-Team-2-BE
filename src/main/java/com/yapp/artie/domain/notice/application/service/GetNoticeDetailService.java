9918cb9dc8ee18279145d0f521c962c89ae369ac
akalswl14 <gnkryd12@gmail.com>
2023-04-20 16:59:41 +0900

    :recycle: refactor: 각 서비스에서 포트를 사용하여 로직을 구성하도록 구현

--- src/main/java/com/yapp/artie/domain/notice/application/service/GetNoticeDetailService.java
+++ src/main/java/com/yapp/artie/domain/notice/application/service/GetNoticeDetailService.java
@@ -2,13 +2,26 @@ package com.yapp.artie.domain.notice.application.service;
 
 import com.yapp.artie.domain.notice.application.port.in.GetNoticeDetailQuery;
 import com.yapp.artie.domain.notice.application.port.in.GetNoticeDetailResponse;
+import com.yapp.artie.domain.notice.application.port.out.LoadNoticePort;
+import com.yapp.artie.domain.notice.domain.Notice;
+import com.yapp.artie.domain.notice.exception.NoticeNotFoundException;
 import com.yapp.artie.global.common.annotation.UseCase;
+import lombok.AllArgsConstructor;
 
 @UseCase
+@AllArgsConstructor
 public class GetNoticeDetailService implements GetNoticeDetailQuery {
+
+  final LoadNoticePort loadNoticePort;
 
   @Override
   public GetNoticeDetailResponse loadNoticeDetail(Long id) {
-    return null;
+    Notice notice = loadNoticePort.loadNoticeDetail(id).orElseThrow(NoticeNotFoundException::new);
+    return new GetNoticeDetailResponse(
+        notice.getId(),
+        notice.getCreatedAt(),
+        notice.getTitle(),
+        notice.getContents()
+    );
   }
 }
--- src/main/java/com/yapp/artie/domain/notice/application/service/GetNoticeListService.java
+++ src/main/java/com/yapp/artie/domain/notice/application/service/GetNoticeListService.java
@@ -2,14 +2,27 @@ package com.yapp.artie.domain.notice.application.service;
 
 import com.yapp.artie.domain.notice.application.port.in.GetNoticeDetailResponse;
 import com.yapp.artie.domain.notice.application.port.in.GetNoticeListQuery;
+import com.yapp.artie.domain.notice.application.port.out.LoadNoticePort;
 import com.yapp.artie.global.common.annotation.UseCase;
 import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 
 @UseCase
+@AllArgsConstructor
 public class GetNoticeListService implements GetNoticeListQuery {
+
+  final LoadNoticePort loadNoticePort;
 
   @Override
   public List<GetNoticeDetailResponse> loadNoticeList() {
-    return null;
+    return loadNoticePort.loadNoticeList().stream()
+        .map(eachNotice ->
+            new GetNoticeDetailResponse(
+                eachNotice.getId(),
+                eachNotice.getCreatedAt(),
+                eachNotice.getTitle(),
+                eachNotice.getContents()
+            )).collect(Collectors.toList());
   }
 }
