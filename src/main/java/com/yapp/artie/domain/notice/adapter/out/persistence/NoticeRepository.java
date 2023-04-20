a20506227a0ca80742f34fb97eafc013b231b99f
akalswl14 <gnkryd12@gmail.com>
2023-04-20 16:58:03 +0900

    :recycle: refactor: 필요없는 메소드 삭제

--- src/main/java/com/yapp/artie/domain/notice/adapter/out/persistence/NoticeRepository.java
+++ src/main/java/com/yapp/artie/domain/notice/adapter/out/persistence/NoticeRepository.java
@@ -1,15 +1,10 @@
 package com.yapp.artie.domain.notice.adapter.out.persistence;
 
-import com.yapp.artie.domain.notice.application.port.in.GetNoticeDetailResponse;
 import com.yapp.artie.domain.notice.domain.Notice;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-  @Query("select new com.yapp.artie.domain.notice.application.port.in.GetNoticeDetailResponse(n.id, n.createdAt, n.title, n.contents) from Notice n")
-  List<GetNoticeDetailResponse> findNoticeDto();
 }
