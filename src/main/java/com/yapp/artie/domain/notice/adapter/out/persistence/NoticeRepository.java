e3b6755f242be365a3ce617c34ebd2c3e5229f47
akalswl14 <gnkryd12@gmail.com>
2023-04-24 13:01:03 +0900

    :recycle: refactor: 불필요한 @Respository 어노테이션 제거
    JPA Repository는 자동으로 빈이 등록되므로, @Respository 어노테이션을 제거함

--- src/main/java/com/yapp/artie/domain/notice/adapter/out/persistence/NoticeRepository.java
+++ src/main/java/com/yapp/artie/domain/notice/adapter/out/persistence/NoticeRepository.java
@@ -2,9 +2,7 @@ package com.yapp.artie.domain.notice.adapter.out.persistence;
 
 import com.yapp.artie.domain.notice.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 interface NoticeRepository extends JpaRepository<Notice, Long> {
 
 }
