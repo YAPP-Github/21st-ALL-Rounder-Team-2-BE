da9eefc0c22eabcd067593936c007600a9de6e07
le2sky <leehaneul990623@gmail.com>
2023-07-14 03:23:43 +0900

    refactor : 카테고리 리포지터리 테스트 패키지 위치 변경

--- src/test/java/com/yapp/artie/category/domain/CategoryRepositoryTest.java
+++ src/test/java/com/yapp/artie/category/domain/CategoryRepositoryTest.java
@@ -1,9 +1,7 @@
-package com.yapp.artie.category.repository;
+package com.yapp.artie.category.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.yapp.artie.category.domain.Category;
-import com.yapp.artie.category.domain.CategoryRepository;
 import com.yapp.artie.gallery.domain.entity.artwork.Artwork;
 import com.yapp.artie.gallery.domain.entity.artwork.Tag;
 import com.yapp.artie.gallery.domain.entity.exhibition.Exhibition;
